db3b1940fcdbeab5087f6fd6be9ce74558f6238d
Gidon Maxime <gidon@b12p4.fil.univ-lille1.fr>
2015-12-10 15:38:37 +0100

    Ajout HomeController

---
+++ ipint15.glp.clientweb/src/main/java/ipint15/glp/webclient/controller/HomeController.java
@@ -0,0 +1,43 @@
+package ipint15.glp.webclient.controller;
+
+import java.util.Locale;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ipint15.glp.api.remote.EtudiantCatalogRemote;
+
+
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	@Inject
+	protected EtudiantCatalogRemote Etudiantbean;
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+	if( Etudiantbean == null ) {
+	logger.info("The bean is not injected !.");
+	return "error";
+	}
+	// Injection works !
+	model.addAttribute("myInjectedBean", Etudiantbean );
+	return "home";
+	}
+	
+}
