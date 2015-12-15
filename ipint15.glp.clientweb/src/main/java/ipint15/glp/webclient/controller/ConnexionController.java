151a8d7d559114e56f02394f5d7ae422f6bdfd31
bahbah <bahbah@b12p2.fil.univ-lille1.fr>
2015-12-15 17:24:26 +0100

    Ajout de la validation si l'adresse mail est utilisee lors de l'inscription et si elle n'existe pas lors de la connexion

--- ipint15.glp.clientweb/src/main/java/ipint15/glp/webclient/controller/ConnexionController.java
+++ ipint15.glp.clientweb/src/main/java/ipint15/glp/webclient/controller/ConnexionController.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
-import org.eclipse.persistence.sessions.server.ServerSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.sun.xml.ws.runtime.dev.Session;
+
 
 import ipint15.glp.api.dto.ConnexionCommand;
 import ipint15.glp.api.dto.EtudiantDTO;
@@ -42,15 +41,18 @@ public class ConnexionController {
 		if (result.hasErrors()) {
 			return "connexion";
 			}
+		if (! etudiantBean.isMailExists(etudiant.getEmail())) {
+			result.rejectValue ("email", null, "Cette adresse mail n'existe pas");
+			return "connexion";
+		}
 		
 		if (etudiantBean.connexion(etudiant.getEmail(), etudiant.getPassword())) {
 			EtudiantDTO etu = etudiantBean.getEtudiant(etudiant.getEmail());
 			HttpSession sessionObj = request.getSession();
 			sessionObj.setAttribute("etudiant", etu);
-			return "profil";
-		} else {
-			return null;
 		}
+			return "profil";
+		
 	}
 
 	/**
