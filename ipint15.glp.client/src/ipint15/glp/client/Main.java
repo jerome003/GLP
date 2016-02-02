a8101a9b93bcc75888f0d582fa8e456bae26441f
bahbah <bahbah@b12p2.fil.univ-lille1.fr>
2016-02-02 17:22:13 +0100

    Correction du type email dans adminModerateur.jsp et ajout du client lourd

--- ipint15.glp.client/src/ipint15/glp/client/Main.java
+++ ipint15.glp.client/src/ipint15/glp/client/Main.java
@@ -1,43 +1,65 @@
 package ipint15.glp.client;
 import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import ipint15.glp.api.dto.Civilite;
+import ipint15.glp.api.dto.GroupeDTO;
+import ipint15.glp.api.dto.ModerateurDTO;
 import ipint15.glp.api.dto.AncienEtudiantDTO;
+import ipint15.glp.api.remote.AdministrationRemote;
 import ipint15.glp.api.remote.AncienEtudiantCatalogRemote;
-import ipint15.glp.api.remote.RechercheRemote;
-
+import ipint15.glp.api.remote.GroupeRemote;
 
 
 
 public class Main {
 
+	private static InitialContext ctx;
+	private static AncienEtudiantCatalogRemote etuBean ;
+	private static GroupeRemote groupBean;
+	private static AdministrationRemote adminBean;
+	private static AncienEtudiantDTO etudiant;
+	private static AncienEtudiantDTO etudiant2;
+	private static AncienEtudiantDTO etudiant3;
+	private static AncienEtudiantDTO etudiant4;
+	private static GroupeDTO groupe;
+	private static GroupeDTO groupe2;
+	private static ModerateurDTO moderateur;
+	private static ModerateurDTO moderateur2;
+
 	public static void main(String[] args) {
 		try {
-			// création du "contexte initial" = de la connexion à l'annuaire du serveur
-						InitialContext context = new InitialContext();
-						// requête sur le nom de la ressource que l'on veut, ici notre EJB
-						AncienEtudiantCatalogRemote catalog =(AncienEtudiantCatalogRemote)context.lookup("ipint15.glp.api.remote.EtudiantCatalogRemote");
-						RechercheRemote recherche =(RechercheRemote)context.lookup("ipint15.glp.api.remote.RechercheRemote");
-						catalog.createEtudiant("Maxime", "Delporte", Civilite.M, "maximus@gmail.com","0654897514","toto", new Date(),"Développeur", "Lille","Wordline","miage",2015);
-						catalog.createEtudiant("Maximus", "Delporte", Civilite.M, "maximus@gmail.com","0568547895","toto", new Date(),"MOA","Lille","CGI","miage",2016);
-						catalog.createEtudiant("Jerome", "Delporto", Civilite.M, "maximus@gmail.com","0565548970","toto", new Date(),"BI","Lille","GCI","miage", 2017);
-						catalog.createEtudiant("Jeromus", "Gideon", Civilite.M, "maximus@gmail.com","0654878950","toto", new Date(),"Dev","Lille","Open","miage", 2018);
-						
-						
-						//List<EtudiantDTO> myPersons = catalog.listEtudiant();
-						List<AncienEtudiantDTO> myRecherche = recherche.rechercherEtudiant("max");
-						Iterator it = myRecherche.iterator();
-						while(it.hasNext()) {
-							System.out.println(it.next().toString());
-						}
-					} catch (NamingException e) {
-						e.printStackTrace();
-					}
-				}
-				public Main() { super(); }
-			}
+
+			ctx= new InitialContext();
+			etuBean = (AncienEtudiantCatalogRemote)ctx.lookup("java:global/ipint15.glp.ear/ipint15.glp.domain/AncienEtudiantCatalogImpl");
+			groupBean = (GroupeRemote)ctx.lookup("java:global/ipint15.glp.ear/ipint15.glp.domain/GroupeImpl");
+			adminBean = (AdministrationRemote) ctx.lookup("java:global/ipint15.glp.ear/ipint15.glp.domain/AdministrationImpl");
+
+
+			moderateur = adminBean.createModerateur("Sarra", "Bahbah", "bahbahsara@yahoo.fr", adminBean.generatePassword(8));
+			moderateur2 = adminBean.createModerateur("Maxime", "Gidon", "gidon.maxime@gmail.com", adminBean.generatePassword(8));
+			
+			groupe = groupBean.createGroupe("Miage","Groupe Miage");
+			groupe2 = groupBean.createGroupe("Elfe", "Ecole des Elfes");
+			
+			adminBean.addGroupetoModo(moderateur.getId(), groupe);
+			adminBean.addGroupetoModo(moderateur2.getId(), groupe2);
+			
+			etudiant =etuBean.createEtudiant("Sarra", "Bahbah", Civilite.Mme, "bahbahsara@yahoo.fr","000000",adminBean.generatePassword(8), 
+					new Date(),"Dev", "Paris", "CGI","miage", 2015, groupe);
+			etudiant2 =etuBean.createEtudiant("Jerome", "Delporte", Civilite.M, "jerome.delporte003@gmail.com","000000", 
+					adminBean.generatePassword(8), new Date(),"Dev", "Lille", "CGI","miage", 2015, groupe);
+			etudiant3 =etuBean.createEtudiant("Maxime", "Gidon", Civilite.M, "gidon.maxime@gmail.com","000000", 
+					adminBean.generatePassword(8), new Date(),"Dev", "Lyon", "Worldline","miage", 2015, groupe2);
+			etudiant4 =etuBean.createEtudiant("Camille", "Marquette", Civilite.M, "camille.marquette1@gmail.com","000000",
+					adminBean.generatePassword(8), new Date(),"Dev", "Lille", "CGI","miage", 2015, groupe2);
+			
+
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+//	public Main() { super(); }
+}
