86467d9a5c201fce7ef1fcbb6f407feaa568d3e8
marquette <marquette@b12p9.fil.univ-lille1.fr>
2016-03-10 13:48:36 +0100

    Ajout connexion CAS prof
    Ajout JSP pour les pages de refus de connexion lorsqu'on se connecte en
    tant que prof avec un compte étudiant et vice versa.
    
    Ajout de la connexion du personnel de Lille 1.

--- ipint15.glp.domain/ejbModule/ipint15/glp/domain/impl/EnseignantCatalogImpl.java
+++ ipint15.glp.domain/ejbModule/ipint15/glp/domain/impl/EnseignantCatalogImpl.java
@@ -2,6 +2,7 @@ package ipint15.glp.domain.impl;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
@@ -38,11 +39,15 @@ public class EnseignantCatalogImpl implements EnseignantCatalogRemote {
 
 	@Override
 	public EnseignantDTO getEnseignantByMail(String mail) {
-		Query q = em.createQuery("select o from Enseignant o WHERE o.email = :email");
-		q.setParameter("email", mail);
+		try {
+		Query q = em.createQuery("select o from Enseignant o WHERE o.mail = :mail");
+		q.setParameter("mail", mail);
 		Enseignant e = (Enseignant) q.getSingleResult();
 		EnseignantDTO dto = e.toEnseignantDTO();
 		return dto;
+		} catch (NoResultException e){
+			return null;
+		}
 	}
 
 	@Override
